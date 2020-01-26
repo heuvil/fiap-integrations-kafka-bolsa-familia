acd9e4361ae4b0199e1866ca256596700c19ec8f
heuvil <heuler@outlook.com>
2020-01-26 18:42:38 -0300

    consumer-3

---
+++ consumer-3/src/main/java/listener/KafkaConsumer.java
@@ -0,0 +1,12 @@
+package listener;
+
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class KafkaConsumer {
+    @KafkaListener(topics = "csv_topic", groupId = "group_id")
+    public void consume(String message){
+        System.out.println("Consumed message:" + message);
+    }
+}
