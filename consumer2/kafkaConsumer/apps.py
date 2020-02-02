800aa6e4f95fe674ff059090f3c438479506e0cd
Rafael Petronilio <rafaelpetronilio@gmail.com>
2020-02-02 13:11:19 -0300

    kafka consumer 2

---
+++ consumer2/kafkaConsumer/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class KafkaconsumerConfig(AppConfig):
+    name = 'kafkaConsumer'
---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\rafae\OneDrive\Área de Trabalho\Estudos\FIAP\Integrations\fiap-integrations-kafka-bolsa-familia\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
